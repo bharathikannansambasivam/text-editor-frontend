7cf5176d556ec60b2fe3ff76e1d0619c71bafb2f
bharathikannansambasivam <91874993+bharathikannansambasivam@users.noreply.github.com>
2026-05-17 09:55:29 +0530

    Update axios base URL for API requests

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://text-editor-backend-5.onrender.com",
+  baseURL: "https://text-editor-backend-bpjy.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
